32cc64e216878ff266ea913a34aa19c7656bf0b3
Tim <Tim280696@hotmail.com>
2021-12-29 16:15:01 +0100

    added redux-persist

--- src/app/store.js
+++ src/app/store.js
@@ -3,17 +3,27 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 import thunk from 'redux-thunk'
 import { userReducer } from './reducers/userReducer'
 import { loginReducer } from './reducers/loginReducer'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
-const reducer = combineReducers({
+const persistConfig = {
+  key: 'root',
+  storage,
+}
+
+const rootReducer = combineReducers({
   userLogin: loginReducer,
   userProfile: userReducer,
 })
 
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 const middleware = [thunk]
 
 const store = createStore(
-  reducer,
+  persistedReducer,
   composeWithDevTools(applyMiddleware(...middleware))
 )
 
+export const persistor = persistStore(store)
 export default store
--- src/index.js
+++ src/index.js
@@ -2,15 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './app/App'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistor } from './app/store'
 
 import store from './app/store'
 
 import './index.css'
 
-
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('root')
 )
